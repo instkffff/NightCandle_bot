ab3ed88481c26731a5581679bfb143f577693d6b
NightCandle <instkffff@gmail.com>
2018-08-16 15:15:56 +0800

    error support

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const bot = require('./modules')
 const log4js = require('./modules/log')
 const logger = log4js.getLogger('message')
+const errlogger = log4js.getLogger('default')
 
 	bot.start((ctx) => ctx.reply('Welcome'))
 	bot.help((ctx) => ctx.reply('Send me a sticker'))
 	bot.on('sticker', (ctx) => ctx.reply('👍'))
 	bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-	bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'))
 
 bot.use((ctx) => {logger.info(ctx.message)})
+bot.catch((err) => {logger.debug(err)})
 
 bot.startPolling()
