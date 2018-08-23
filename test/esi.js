aee8e33c140a70bf8ffd9731030ad67ffbb19b52
NightCandle <instkffff@gmail.com>
2018-08-23 14:53:16 +0800

    update test

--- test/esi.js
+++ test/esi.js
@@ -1,6 +1,6 @@
 // The main module returns a default Api instance with an attached
 // Api constructor if configuration changes are necessary.
-let esi = require('eve-swagger');
+let esi = require('eve-swagger')
 
 // Creating a new Api instance with a different configuration.
 // All options, with their default values, are shown below.
@@ -12,11 +12,26 @@ let esi2 = esi({
     timeout: 6000,
     minTime: 0,
     maxConcurrent: 0
-  });
+  })
 
 // Fetch all active alliance ids (could also call 'esi.alliances.all()')
-esi.alliances().then(result => {
-  console.log(result);
+
+esi.alliances(1695357456).corporations().then(result => {
+  
+  for (let i=1; i<=result.length; i++){
+    esi.corporations(result[i]).info().then(info =>{
+      ticker=info.ticker
+      name=info.corporation_name
+      id=result[i] 
+      let corpName = {
+        id:id,
+        Ticker:ticker,
+        Name:name
+      }
+      console.log(corpName)
+    })
+  } 
 }).catch(error => {
-  console.error(error);
-});
+  console.error(error)
+})
+
